45edc6878d4dd04a6bde08e8874fda2a530e7462
K Muralidharan <murali-k42@outlook.com>
2020-05-04 20:05:55 +0530

    Binary Search

--- binarysearch.py
+++ binarysearch.py
@@ -1,22 +1,25 @@
 def binary_search(input_array, find_num, start_idx):
-    print(input_array)
+
     n = len(input_array)
     curr_idx = 0
+    is_found = False
     if n > 1:
         mid_num = input_array[int(n/2)]
         if find_num < mid_num:
-            curr_idx = start_idx + binary_search(input_array[0:int(n/2)], find_num, 0)
+            curr_idx, is_found = binary_search(input_array[0:int(n/2)], find_num, 0)
+            curr_idx += start_idx
         elif find_num > mid_num:
-            curr_idx = start_idx + binary_search(input_array[int(n/2)+1:], find_num, int(n/2)+1)
+            curr_idx, is_found = binary_search(input_array[int(n/2)+1:], find_num, int(n/2)+1)
+            curr_idx += start_idx
         else:
             curr_idx = start_idx + int(n/2)
+            is_found = True
     else:
-        if input_array[0] == find_num:
-            curr_idx = start_idx
-        else:
-            curr_idx = float("NaN")
+        curr_idx = start_idx
+    if input_array[0] == find_num:
+        is_found = True
 
-    return curr_idx
+    return curr_idx, is_found
 
 # Initial section will be migrated to a common input file at a later date, with user input for filename and delimiter
 
@@ -35,4 +38,11 @@ for sublist in file_dat:
 print(inp_arr)
 num = input("Enter number to search for: ")
 
-print(f"{num} found at location {binary_search(inp_arr, int(num), 0)}")
+idx, found = binary_search(inp_arr, int(num), 0)
+
+if found:
+    print(f"{num} is found at location {idx} in the input array")
+else:
+    print(f"{num} is not found in array, closest match at location {idx} is {inp_arr[idx]}")
+
+
