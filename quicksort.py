57cb37af1016d720cc55a194ed344bbc41bd7c82
K Muralidharan <murali-k42@outlook.com>
2020-05-07 19:41:11 +0530

    Added ability for user to add input outside of editing the file

--- quicksort.py
+++ quicksort.py
@@ -39,7 +39,9 @@ def quick_sort(input_array, l, r):
 
 if __name__ == '__main__':
 
-    file_nm = "sortfil.csv"
+    file_nm = input("Enter csv filename with data: ")
+    if file_nm == "":
+        file_nm = "sortfil.csv"
 
     with open(file_nm, "r") as f:
         file_dat = f.readlines()
